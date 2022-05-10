254e265089bda84817af88bac672157a7f756652
kevinYu12138 <yu18297682878@126.com>
2022-05-10 17:31:16 +0800

    dell-3

--- PRF.py
+++ PRF.py
@@ -10,13 +10,13 @@ print("Correct segmentation has ", len(correct), "words.")
 print("\nStart loading rmm_segment text ...")
 for line in rmm_segment.readlines():  # 加载rmm分词结果
     my_segment.append(line)
-print("RMM_segmentation has ", len(my_segment), "words.")
+print("RMM_segmentation has ", len(my_segment), "words.\n")
 
 seg_correct = 0  # 正确分词个数
 for i in range(len(my_segment)):
     if my_segment[i] in correct:
         seg_correct += 1
-print("\n", seg_correct, "correct words in rmm_segmentation.")
+print(seg_correct, "correct words in rmm_segmentation.\n")
 
 precision = seg_correct / len(my_segment)
 recall = seg_correct / len(correct)
